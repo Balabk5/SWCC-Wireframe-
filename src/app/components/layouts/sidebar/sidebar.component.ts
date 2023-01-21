9150d47c47171b66f9167ff739d82a233f5bb8cd
vinayakbhat430kaar <vinayakbhat430.kaar@gmail.com>
2023-01-21 13:21:08 +0530

    sidebar changes

--- src/app/components/layouts/sidebar/sidebar.component.ts
+++ src/app/components/layouts/sidebar/sidebar.component.ts
@@ -8,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
 
 }
