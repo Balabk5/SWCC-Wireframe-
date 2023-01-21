08a73e92ec26ba33724625663e49dc72d5f0e423
vinayakbhat430kaar <vinayakbhat430.kaar@gmail.com>
2023-01-21 11:26:04 +0530

    added hr request dashboard screen

---
+++ src/app/components/hr-request-dashboard/hr-request-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hr-request-dashboard',
+  templateUrl: './hr-request-dashboard.component.html',
+  styleUrls: ['./hr-request-dashboard.component.scss']
+})
+export class HrRequestDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
