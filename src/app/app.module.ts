e0a162df741a3fe25952d65e9f7fc3ca15a2c7fb
poojasri2022 <poojasriramkumar2@gmail.com>
2023-01-21 12:12:29 +0530

    finance request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,19 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
 import { ItComponent } from './it/it.component';
 import { FinanceComponent } from './finance/finance.component';
 import { ProcurementComponent } from './procurement/procurement.component';
-<<<<<<< HEAD
+
 import { FinanceRequestComponent } from './finance-request/finance-request.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-=======
+
 import { JobrequestComponent } from './jobrequest/jobrequest.component';
 import { HrNewRequestsComponent } from './hr-new-requests/hr-new-requests.component';
 import { HrOnholdRequestsComponent } from './hr-onhold-requests/hr-onhold-requests.component';
 import { HrCompletedRequestsComponent } from './hr-completed-requests/hr-completed-requests.component';
 import { HrInprogressRequestsComponent } from './hr-inprogress-requests/hr-inprogress-requests.component';
 
->>>>>>> 10e3d9d0bcee0eddd50532de89e137c00ab00245
+
 @NgModule({
   imports: [
     BrowserAnimationsModule,
@@ -42,15 +42,15 @@ import { HrInprogressRequestsComponent } from './hr-inprogress-requests/hr-inpro
     ItComponent,
     FinanceComponent,
     ProcurementComponent,
-<<<<<<< HEAD
-    FinanceRequestComponent
-=======
+
+    FinanceRequestComponent,
+
     JobrequestComponent,
     HrNewRequestsComponent,
     HrOnholdRequestsComponent,
     HrCompletedRequestsComponent,
     HrInprogressRequestsComponent,
->>>>>>> 10e3d9d0bcee0eddd50532de89e137c00ab00245
+
 
   ],
   providers: [],
